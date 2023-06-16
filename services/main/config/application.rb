486cea5ba20bb9f8cfd73b423d9bd58238cfa6a6
Maciej Panek <maciejpa@student.agh.edu.pl>
2023-06-16 21:35:26 +0200

    Adding kibana

--- services/main/config/application.rb
+++ services/main/config/application.rb
@@ -30,8 +30,7 @@ module DockerRailsLogstash
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
-
+    config.load_defaults 7.0
     config.lograge.enabled = true
     config.lograge.formatter = Lograge::Formatters::Logstash.new
     config.lograge.logger = LogStashLogger.new(type: :udp, host: ENV['LOGSTASH_HOST'], port: 8089)
