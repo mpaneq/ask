5c77220bd5095d305995fd7176e20434db8cdaf2
Maciej Panek <maciejpa@student.agh.edu.pl>
2023-06-16 11:28:25 +0200

    Configuring logstast

--- services/main/config/application.rb
+++ services/main/config/application.rb
@@ -20,3 +20,20 @@ module Main
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
+module DockerRailsLogstash
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
+
+    config.lograge.enabled = true
+    config.lograge.formatter = Lograge::Formatters::Logstash.new
+    config.lograge.logger = LogStashLogger.new(type: :udp, host: ENV['LOGSTASH_HOST'], port: 8089)
+  end
+end
